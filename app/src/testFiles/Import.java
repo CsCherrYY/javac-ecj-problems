f58194e62a998ae8cb6a4bd34c80d2795c49d563
Shi Chen <chenshi@microsoft.com>
2023-01-12 10:50:03 +0800

    upd

--- app/src/testFiles/Import.java
+++ app/src/testFiles/Import.java
@@ -1,12 +1,20 @@
 import java.util.List;
 import java.awt.List;
-// package not exist: compiler.err.doesnt.exist
+// package not exist
+// javac: compiler.err.doesnt.exist
+// ecj: CannotImportPackage
 import java.util;
-// package exists and type not exist: compiler.err.cant.resolve.location
+// package exists and type not exist
+// javac: compiler.err.cant.resolve.location
+// ecj: CannotImportPackage
 import java.util.zip;
-// package not exist: compiler.err.doesnt.exist
+// package not exist
+// javac: compiler.err.doesnt.exist
+// ecj: ImportNotFound
 import java.NotExist;
-// package exists and type not exist: compiler.err.cant.resolve.location
+// package exists and type not exist
+// javac: compiler.err.cant.resolve.location
+// ecj: ImportNotFound
 import java.util.NotExist;
 
 public class Import {
